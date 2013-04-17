a79afefb71b5e7d954fb7fdc52045a9f927014ab
Justin Ruggles <justin.ruggles@gmail.com>
2013-04-16 23:53:02 -0400

    webp: handle 'VP8X' and other known chunk types

--- libavcodec/webp.c
+++ libavcodec/webp.c
@@ -31,13 +31,15 @@ typedef struct WebPContext {
 
 static int vp8_lossless_decode_frame(AVCodecContext *avctx, AVFrame *p,
                                      int *got_frame, uint8_t *data_start,
-                                     int data_size)
+                                     unsigned int data_size)
 {
     int ret;
 
     avctx->width   = 800;
     avctx->height  = 600;
     avctx->pix_fmt = AV_PIX_FMT_ARGB;
+
+    av_log(avctx, AV_LOG_DEBUG, "VP8 Lossless\n");
 
     if ((ret = ff_get_buffer(avctx, p, 0)) < 0) {
         av_log(avctx, AV_LOG_ERROR, "get_buffer() failed\n");
@@ -47,20 +49,29 @@ static int vp8_lossless_decode_frame(AVCodecContext *avctx, AVFrame *p,
     memset(p->data[0], 0, p->linesize[0] * avctx->height);
 
     *got_frame = 1;
+    p->pict_type = AV_PICTURE_TYPE_I;
+    p->key_frame = 1;
 
     return data_size;
 }
 
 static int vp8_lossy_decode_frame(AVCodecContext *avctx, AVFrame *p,
                                   int *got_frame, uint8_t *data_start,
-                                  int data_size)
+                                  unsigned int data_size)
 {
     WebPContext *s = avctx->priv_data;
     AVPacket pkt;
 
+    av_log(avctx, AV_LOG_DEBUG, "VP8 Lossy\n");
+
     if (!s->initialized) {
         vp8_decode_init(avctx);
         s->initialized = 1;
+    }
+
+    if (data_size > INT_MAX) {
+        av_log(avctx, AV_LOG_DEBUG, "unsupported chunk size\n");
+        return AVERROR_PATCHWELCOME;
     }
 
     av_init_packet(&pkt);
@@ -74,9 +85,11 @@ static int webp_decode_frame(AVCodecContext *avctx, void *data, int *got_frame,
                              AVPacket *avpkt)
 {
     AVFrame * const p = data;
-    int ret = 0;
     GetByteContext g;
+    int ret;
     unsigned int chunk_type, chunk_size;
+
+    *got_frame = 0;
 
     bytestream2_init(&g, avpkt->data, avpkt->size);
 
@@ -97,41 +110,59 @@ static int webp_decode_frame(AVCodecContext *avctx, void *data, int *got_frame,
         return AVERROR_INVALIDDATA;
     }
 
-    chunk_type = bytestream2_get_le32(&g);
-    chunk_size = bytestream2_get_le32(&g);
+    while (bytestream2_get_bytes_left(&g) > 0) {
+        chunk_type = bytestream2_get_le32(&g);
+        chunk_size = bytestream2_get_le32(&g);
+        if (chunk_size == UINT32_MAX)
+            return AVERROR_INVALIDDATA;
+        chunk_size += chunk_size & 1;
 
-    if (bytestream2_get_bytes_left(&g) < chunk_size || chunk_size > INT_MAX)
-        return AVERROR_INVALIDDATA;
+        if (bytestream2_get_bytes_left(&g) < chunk_size)
+            return AVERROR_INVALIDDATA;
 
-    switch (chunk_type) {
-    case MKTAG('V', 'P', '8', ' '):
-        ret = vp8_lossy_decode_frame(avctx, p, got_frame,
-                                     avpkt->data + bytestream2_tell(&g),
-                                     chunk_size);
-        if (ret < 0)
-            return ret;
-        break;
-    case MKTAG('V', 'P', '8', 'L'):
-        ret = vp8_lossless_decode_frame(avctx, p, got_frame,
-                                        avpkt->data + bytestream2_tell(&g),
-                                        chunk_size);
-        if (ret < 0)
-            return ret;
-        break;
-    case MKTAG('V', 'P', '8', 'X'):
-        av_log(avctx, AV_LOG_ERROR, "VP8X is not implemented\n");
-        return AVERROR_PATCHWELCOME;
-    default:
-        av_log(avctx, AV_LOG_ERROR, "invalid WebP image type\n");
-        return AVERROR_INVALIDDATA;
+        switch (chunk_type) {
+        case MKTAG('V', 'P', '8', ' '):
+            if (!*got_frame) {
+                ret = vp8_lossy_decode_frame(avctx, p, got_frame,
+                                             avpkt->data + bytestream2_tell(&g),
+                                             chunk_size);
+                if (ret < 0)
+                    return ret;
+            }
+            break;
+        case MKTAG('V', 'P', '8', 'L'):
+            if (!*got_frame) {
+                ret = vp8_lossless_decode_frame(avctx, p, got_frame,
+                                                avpkt->data + bytestream2_tell(&g),
+                                                chunk_size);
+                if (ret < 0)
+                    return ret;
+            }
+            break;
+        case MKTAG('V', 'P', '8', 'X'):
+            break;
+        case MKTAG('A', 'L', 'P', 'H'):
+            av_log(avctx, AV_LOG_WARNING, "skipping unsupported alpha chunk\n");
+            break;
+        case MKTAG('I', 'C', 'C', 'P'):
+        case MKTAG('A', 'N', 'I', 'M'):
+        case MKTAG('A', 'N', 'M', 'F'):
+        case MKTAG('E', 'X', 'I', 'F'):
+        case MKTAG('X', 'M', 'P', ' '):
+            av_log(avctx, AV_LOG_VERBOSE, "skipping unsupported chunk\n");
+            break;
+        default:
+            av_log(avctx, AV_LOG_WARNING, "skipping unknown chunk\n");
+            break;
+        }
+        bytestream2_skip(&g, chunk_size);
     }
 
-    if (*got_frame) {
-        p->pict_type = AV_PICTURE_TYPE_I;
-        p->key_frame = 1;
+    if (!*got_frame) {
+        av_log(avctx, AV_LOG_ERROR, "image data not found\n");
+        return AVERROR_INVALIDDATA;
     }
-
-    return ret;
+    return avpkt->size;
 }
 
 
